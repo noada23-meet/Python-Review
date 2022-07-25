de29bb5bce4efa7e24de368bf870cd937a6034c3
noada23-meet <109904933+noada23-meet@users.noreply.github.com>
2022-07-25 23:02:23 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,8 +1,31 @@
 def create_youtube_video(title, description):
-	youtube_video={"title":title,"description":description,"likes":0,"dislikes":0,"username":comments}
+	youtube_video = {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}}
 	return youtube_video
 
 def like(youtube_video):
-	if like is youtube_video:
-		youtube_video("likes")+=1
-		return youtube_video
+	if "likes" in youtube_video:
+		youtube_video["likes"] += 1
+
+def dislike(youtube_video):
+	if "dislikes" in youtube_video:
+		youtube_video["dislikes"] += 1
+
+def add_comment(youtubevideo, username, comment_text):
+	youtubevideo['comments'] = {username, comment_text}
+	return youtubevideo
+
+
+def main():
+	new_youtube_dict = create_youtube_video("meet 2022", "Summer")
+	like(new_youtube_dict)
+	dislike(new_youtube_dict)
+	add_comment(new_youtube_dict, "niv", "I liked your video")
+	print("Title: ", new_youtube_dict['title'])
+	print("Description: ", new_youtube_dict['description'])
+	print(new_youtube_dict['likes'], " people liked this :)")
+	print(new_youtube_dict['dislikes'], " people disliked this :(")
+	print("Comments: \n==========")
+	print(new_youtube_dict['comments'])
+
+if __name__ == '__main__':
+	main()
