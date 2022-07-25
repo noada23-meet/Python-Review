559e3d826c0bf8f53ccd50091a9c5b36e0a8e54d
noada23-meet <109904933+noada23-meet@users.noreply.github.com>
2022-07-25 18:37:18 +0300

    pythonReview

---
+++ pythonReview.py
@@ -0,0 +1,8 @@
+def create_youtube_video(title, description):
+	youtube_video={"title":title,"description":description,"likes":0,"dislikes":0,"username":comments}
+	return youtube_video
+
+def like(youtube_video):
+	if like is youtube_video:
+		youtube_video("likes")+=1
+		return youtube_video
